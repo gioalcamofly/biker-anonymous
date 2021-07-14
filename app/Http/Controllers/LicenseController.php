23be3568ad13842da40ce496e75b2fdb341723cf
gcabrera <gcabrera@opencanarias.es>
2021-07-14 18:33:07 +0100

    pdf download

--- app/Http/Controllers/LicenseController.php
+++ app/Http/Controllers/LicenseController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\License;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use PDF;
 
 class LicenseController extends Controller
 {
@@ -19,6 +21,17 @@ class LicenseController extends Controller
         $path = $request->file('file')->store(Carbon::now()->format('d-m-Y'));
 
         return response('File Uploaded', 200);
+
+    }
+
+    public function getLicensePdf(Request $request) {
+
+        $license = License::where('license_number', $request->input('license_number'))->first();
+
+        view()->share('license', $license);
+        $pdf = PDF::loadView('license', $license);
+
+        return $pdf->download('license.pdf');
 
     }
 
